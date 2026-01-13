2544aee865709fdcc9baaa0061530f7748fb5aaf
mlbench-lhr <abdul.rafay@ml-bench.com>
2026-01-13 15:25:45 +0500

    corrected build

--- src/app/(other_pages)/Veterinarian/(others-pages)/patient/new_patient/page.tsx
+++ src/app/(other_pages)/Veterinarian/(others-pages)/patient/new_patient/page.tsx
@@ -1,10 +1,12 @@
 import AddPatientGuardian from '@/components/Veterinarian/new_patient/index'
 import AddPatientMultiStep from '@/components/Veterinarian/new_patient/new_patient'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 function page() {
   return (
-    <AddPatientMultiStep />
+    <Suspense fallback={<div>Loading...</div>}>
+      <AddPatientMultiStep />
+    </Suspense>
   )
 }
 
