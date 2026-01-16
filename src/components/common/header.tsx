bce763535115204a51b1ead0e3e494c305020f34
ahmadraza-mlbench <ahmad_raza@ml-bench.com>
2026-01-16 16:13:47 +0500

    patient detail detsign

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from 'react';
-import { ArrowLeft, Search, Bell, Edit2, Eye, Download, Plus } from 'lucide-react';
+import { ArrowLeft, Search, Bell, Edit2, Eye, Download, Plus, ChevronLeft } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
 // Header Component
@@ -15,23 +15,23 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
     };
     return (
         <div className="flex items-center justify-between p-4 ">
-            <div className="flex items-center gap-3">
+            <div className="flex items-center gap-3 w-full justify-center">
                 <button
                     onClick={onBack}
-                    className="w-8 h-8 rounded-full bg-gray-100 flex items-center justify-center hover:bg-gray-200"
+                    className="w-fit h-fit rounded-full bg-gray-10 flex absolute left-4 items-center justify-center hover:bg-gray-200"
                 >
-                    <ArrowLeft size={20} className="text-gray-700" />
+                    <ChevronLeft size={20} className="text-gray-700" />
                 </button>
-                <h1 className="text-xl font-bold text-primary">{title}</h1>
+                <h1 className="text-base font-medium">{title}</h1>
             </div>
-            <div className="flex items-center gap-3">
+            {/* <div className="flex items-center gap-3">
                 <button className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center hover:bg-gray-200">
                     <Search size={20} className="text-primary" />
                 </button>
                 <button className="w-10 h-10 rounded-full bg-primary flex items-center justify-center hover:bg-blue-700">
                     <Bell size={20} className="text-white" />
                 </button>
-            </div>
+            </div> */}
         </div>
     );
 };
